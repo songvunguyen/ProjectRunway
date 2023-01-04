8049eb4a32542cdb7dffc40afb8369bf9a10a287
Song Vu Nguyen <vunnems08@gmail.com>
2023-01-04 12:18:10 -0500

    Finished player movements

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,12 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     float speed = 10f;
-    float jumpHeight = 10f;
+    float jumpHeight = 5f;
     Rigidbody2D rb;
     Vector2 moveVal;
     Animator ani;
     SpriteRenderer sprite;
+    bool isJumping = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,11 +29,24 @@ public class PlayerController : MonoBehaviour
     
     private void FixedUpdate() {
         if(moveVal != Vector2.zero){
-            rb.velocity = new Vector2(moveVal.x * speed, rb.velocity.y); 
-            ani.SetFloat("Speed", Mathf.Abs(rb.velocity.magnitude));   
+            if(!isJumping){
+                rb.velocity = new Vector2(moveVal.x * speed, moveVal.y * jumpHeight); 
+                ani.SetFloat("Speed", Mathf.Abs(rb.velocity.magnitude));  
+                ani.SetBool("IsJumping", isJumping);
+            }else{
+                //if player is in the air, half the speed they can move
+                rb.velocity = new Vector2(moveVal.x * speed/2, rb.velocity.y);  
+            }
+
+            if(moveVal.y > 0 && isJumping != true){
+                isJumping = true;
+                ani.SetBool("IsJumping", isJumping);
+            } 
+            
         }else{
-            rb.velocity = new Vector2(moveVal.x * speed, rb.velocity.y); 
+            rb.velocity = new Vector2(0, rb.velocity.y); 
             ani.SetFloat("Speed", 0);
+            ani.SetBool("IsJumping", isJumping);
         }
 
         if(moveVal.x == 1){ 
@@ -45,5 +59,11 @@ public class PlayerController : MonoBehaviour
 
     void OnMove(InputValue value){
         moveVal = value.Get<Vector2>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag == "Ground"){
+            isJumping = false;
+        }
     }
 }
